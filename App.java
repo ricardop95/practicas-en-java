fed72bc7c691d5efb754f0b35be14dc2f55038aa
Ricardo Peña <107850686+ricardop95@users.noreply.github.com>
2022-08-02 18:12:59 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args){
+
+        Empleado emp = new Empleado();
+        emp.salariominimo=200;
+        System.out.println("El salario minimo del empleado es"+ emp.salariominimo);
+    }
+    
+}
---
+++ Empleado.java
@@ -0,0 +1,35 @@
+public class Empleado {
+    private String nombre;
+    private int Salario;
+    int salariominimo;
+{
+public Empleado()
+    {
+        this.nombre="";
+        this.Salario=0;
+        this.salariominimo=600;
+    }
+public String getNombre(){
+    return this.nombre;
+}
+
+public void setNombre(String nom){
+    this.Nombre=nom;
+}
+public int getSalario(){
+    return this.Salario;
+}
+public void setSalario(int Sal){
+    this.Salario=Sal;
+ if (Sal<0){
+ }
+     else if (Sal<salariominimo);
+     this.Salario=salariominimo;
+
+ }   
+
+    }
+    
+
+
+    }
